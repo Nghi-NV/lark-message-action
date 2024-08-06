3b9c454207bf45b462b842b7c769da47de02a645
Nghi-NV <canary020990@gmail.com>
2024-08-06 17:15:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+
+# lark-message-action
+Github action that send message to Lark
+
+# Inputs
+
+
+## ``lark-bot-notify-webhook``
+Required: **YES**.  
+
+Lark Bot Notify Webhook
+
+## ``title``
+Required: **YES**.  
+
+Notify title
+
+## ``content``
+Required: **YES**.  
+
+Notify content
+
+## ``header-color``
+Required: **No**.  
+Default: **green**
+
+Notify header color
+
+
+# Usage Example
+
+## Simple Workflow
+
+```yaml
+# .github/workflows/main.yml
+name: Main
+on: [push]
+
+jobs:
+  my_job:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v3
+
+      - name: Upload to Next Cloud
+        uses: Nghi-NV/lark-message-action@v1
+        with:
+          title: "Notification"
+          content: "Hello"
+          header-color: "red"
+          lark-bot-notify-webhook: ${{ secrets.LARK_BOT_NOTIFY_WEBHOOK }}
+          
+```
---
+++ scripts/send_message.py
@@ -0,0 +1,66 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+import json
+import argparse
+
+def build_card_message():
+    elements = [
+        {
+            "tag": "column_set",
+            "flex_mode": "none",
+            "background_style": "default",
+            "columns": [
+                {
+                    "tag": "column",
+                    "width": "weighted",
+                    "weight": 1,
+                    "vertical_aign": "top",
+                    "elements": [
+                        {
+                            "tag": "div",
+                            "text": {
+                                "content": args.content,
+                                "tag": "lark_md"
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+
+    message_card = {
+        "msg_type": "interactive",
+        "card": {
+            "config": {
+                "wide_screen_mode": False
+            },
+            "header": {
+                "title": {
+                    "tag": "plain_text",
+                    "content": args.title
+                },
+                "template": args.header_color
+            },
+            "elements": elements
+        }
+    }
+
+    message_card_json = json.dumps(message_card, indent=4, ensure_ascii=False)
+    return message_card_json
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Notify Lark')
+    parser.add_argument('--title', type=str, help='Title', required=True)
+    parser.add_argument('--content', type=str, help='Content', required=True)
+    parser.add_argument('--header-color', type=str, help='Header color', default="green")
+    parser.add_argument('--lark-bot-notify-webhook', type=str, help='Lark webhook', required=True)
+    args = parser.parse_args()
+
+    message = build_card_message(args)
+    print("Notify message: ", message)
+    subprocess.run(["curl", "-X", "POST", "-H", "Content-Type: application/json", "-d", message, args.lark_bot_notify_webhook], check=True)
+    print("Notify message sent succeed")
